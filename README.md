9ca6769795f7e7be5a8a0e2bf5c05366b6fa32e7
Neil Ritter <drazi@Neils-MacBook-Air.local>
2023-12-21 16:10:54 +0530

    Initial site structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ak
+# Accumulated Knowledge POC
+
+Web based tool to build and tell stories with the help of gAI.
+
---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['ritter.neil@gmail.com']
+    POSTS_PER_PAGE = 10
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
